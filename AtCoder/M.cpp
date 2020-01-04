a1b1e61fefd8eed5abcdaebfadb7e7de2a5e7b0c
Chitrang Srivastava <chitrang.srivastava@gmail.com>
2020-01-04 17:03:07 +0530

    Optimized to O(n^2)
    Much faster as compared to previous version but still fails in last few test cases with WA.

--- AtCoder/M.cpp
+++ AtCoder/M.cpp
@@ -2,37 +2,43 @@
 #include <algorithm>
 
 using namespace std;
-#define MAX (1000000007)
+const int MAX = 1e9 + 7;
 int main(int argc, char*argv[])
 {
 	int N, K;
 	cin >> N >> K;
 	vector<int> a(N+1);
 	sort(a.begin(), a.end());
-	vector<vector<int>> dp(N+1, vector<int>(K+1, 0));
+	vector<vector<pair<int, int>>> dp(N+1, vector<pair<int, int>>(K+1, make_pair(0, 0)));
 	for (int i = 1; i <= N; ++i)
 		cin >> a[i];
 	
-	dp[0][0] = 1;
-	//dp[1][0] = 1; // base case i.e. 1st child with
-	for (int i = 1 ; i <= N; ++i) //child loop
+	for (int i = min(a[1],K) ; i >= 0; --i)
+	{dp[1][i].first = 1;dp[1][i].second = (dp[1][i].first + dp[1][i+1].second)%MAX;}
+	
+	for (int i = 2 ; i <= N; ++i) //child loop
 	{
-		for (int j = K; j >= 0 ; --j)
+		for (int j = K; j >= 0 ; --j) // candy loop
 		{
 			int left = a[i] > j ? 0: j-a[i];
-			if(dp[i-1][left])
+			
+			if(dp[i-1][left].first)
 			{
-				for(int k = min(a[i], j) ; k>=0; --k)
+				if(j)
 				{
-					//if(j<k)
-					//	continue;
-					if(dp[i-1][j-k])
-						dp[i][j] = (dp[i][j] + dp[i-1][j-k])%MAX;
-					else
-						break;
+					dp[i][j].first += (dp[i-1][left].second)%MAX;
+					if(a[i]-j)
+					{
+						dp[i][j].first = (dp[i][j].first - dp[i-1][K-(a[i]-j)+1].second + MAX)%MAX;
+						if(dp[i][j].first<0)
+							dp[i][j].first += MAX;
+					}
 				}
+				else
+					dp[i][j].first = 1;
+				dp[i][j].second = (dp[i][j].first + dp[i][j+1].second)%MAX;
 			}
 		}
 	}
-	cout << dp[N][K];
+	cout << dp[N][K].first;
 }
