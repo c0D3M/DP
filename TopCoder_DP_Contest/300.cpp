afe3469bf61a22557ed041b5af7b3e36a9867544
Chitrang Srivastava <chitrang.srivastava@gmail.com>
2020-05-16 20:35:04 +0530

    Create 300.cpp
    Program: Minimum copy-paste operation reach 'n' smily.
    Each prime 'n' can be achieved in prime number of operation.
    Idea: Prime factors of 2^3 * 5^3 = 2*3 + 5*3 = 21

---
+++ TopCoder_DP_Contest/300.cpp
@@ -0,0 +1,31 @@
+#include <bits/stdc++.h>
+#include <algorithm>
+using namespace std;
+class EmoticonsDiv2
+{
+public:
+int printSmiles(int n)
+{
+
+   int sum = 0;
+    while (n % 2 == 0)  
+    {  
+        sum += 2;
+        n = n/2;  
+    }  
+  
+    for (int i = 3; i <= sqrt(n); i = i + 2)  
+    {  
+        // While i divides n, print i and divide n  
+        while (n % i == 0)  
+        {  
+            sum += i;
+            n = n/i;  
+        }  
+    }  
+  
+    if (n > 2)  
+		sum += n;
+    return sum;
+}  
+};
